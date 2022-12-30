843ffa59681a15829caf5cc5ee81fb8ddc9751a4
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-12-30 17:08:48 -0300

    criação de estilo global para aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import styles from "../styles/styles.module.scss"
+import styles from '../styles/styles.module.scss';
+import Head from 'next/head';
 
-export default function Home() { 
-  return ( 
-    <div> 
-      <h1 className={styles.title}> 
-        Primeiro projeto NextJs 
-      </h1> 
-    </div>  
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Board - Organização de tarefas</title>
+      </Head>
+
+      <div>
+        <h1 className={styles.title}>Primeiro projeto NextJs</h1>
+      </div>
+    </>
   );
 }
