3e5fe9bb6a3b50e3734cc5518b727ca7d2c09e13
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-12-31 10:47:02 -0300

    criação de novo componente input para reutilização em outras páginas

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export default function Input({ ...rest }) {
+  return (
+    <div className={styles.containerInput}>
+      <input {...rest} />
+      <button>
+        <img src="/images/addIcon.svg" alt="Botão de adicionar" />
+      </button>
+    </div>
+  );
+}
--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import styles from './styles.module.scss';
+import Input from '../../components/Input';
 
 export default function Board() {
   return (
-    <main>
-      <h1>Página Tarefas</h1>
-    </main>
+    <>
+      <Head>
+        <title>Minhas Tarefas - Board</title>
+      </Head>
+
+      <main className={styles.container}>
+        <Input type="text" placeholder="Qual sua Tarefa?" />
+      </main>
+    </>
   );
 }
